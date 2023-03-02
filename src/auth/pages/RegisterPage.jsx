a9f18981185e8c9315edb730f061d8abae6babbc
Luis Fereira <luis@spartandevs.com>
2023-03-02 15:04:42 -0400

    [LFereira] adds components JournalLayout and JournalPages

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,68 @@
-
+import {  Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { minHeight } from "@mui/system";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPAge</div>
-  )
-}
+    <AuthLayout title='Crear Cuenta'>
+       <form>
+          <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="nombre de usuario"
+                type="text"
+                placeholder="xiragon"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="nombre completo"
+                type="text"
+                placeholder="Luis fereira"
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="contraseña"
+                type="password"
+                placeholder="contraseña"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="repite tu contraseña"
+                type="password"
+                placeholder="contraseña"
+                fullWidth
+              />
+            </Grid>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12} >
+                <Button variant="contained" fullWidth>
+                  Crear cuenta
+                </Button>
+              </Grid>
+
+            </Grid>
+
+            <Grid container direction="row" justifyContent="end">
+              <Typography sx={{ mr: 1 }}> ¿Ya tienes cuenta?</Typography>
+              <Link component={ RouterLink } color= 'inherit' to='/auth/login'>
+              ingresar
+              </Link>
+
+            </Grid>
+
+          </Grid>
+        </form>
+    </AuthLayout>
+       
+  );
+};
+
--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -1,12 +1,15 @@
 import { Typography } from "@mui/material"
-import { MailOutline } from "@mui/icons-material"
+
+import { JournalLayout } from "../JournalLayout"
 
 
 export const JournalPages = () => {
   return (
-    <>
-    <Typography variant='h1'>JournalPages</Typography>
-    <MailOutline/>
-    </>
+    <JournalLayout>
+    <Typography> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor a odit ab sit laborum illum, impedit alias dicta cum. Maxime vel fuga repellendus sed dolores sunt necessitatibus incidunt unde magnam?</Typography>
+
+    {/* NothinSelected */}
+    {/* NoteView */}
+    </JournalLayout>
   )
 }
